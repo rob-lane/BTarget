439d1d57c947db0d317bfa1096abfda48ed1f1c9
r.lane <r.lane@fluidappsdev.com>
2012-12-08 18:25:40 -0800

    Initial app generated

---
+++ BTarget/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  BTarget
+//
+//  Created by Robert Lane on 12/8/12.
+//  Copyright Fluid Apps LLC 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ BTarget/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BTarget
+//
+//  Created by Robert Lane on 12/8/12.
+//  Copyright Fluid Apps LLC 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
